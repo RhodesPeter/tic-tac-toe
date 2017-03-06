d809194b4b6836852dcd33cc3b3cfda4ffb41ec5
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-03-06 00:11:48 +0000

    Add tests for the get_pattern_1_move() function, related #5

--- game.js
+++ game.js
@@ -1,16 +1,17 @@
  "use strict";
 
-var patterns_1 = [
-                  [(/ OO....../),0],[(/O..O.. ../),6], [(/......OO /),8],
-                  [(/.. ..O..O/),2], [(/ ..O..O../),0],[(/...... OO/),6],
-                  [(/..O..O.. /),8],[(/OO ....../),2], [(/ ...O...O/),0],
-                  [(/..O.O. ../),6], [(/O...O... /),8],[(/.. .O.O../),2],
-                  [(/O O....../),1],[(/O.. ..O../),3], [(/......O O/),7],
-                  [(/..O.. ..O/),5], [(/. ..O..O./),1],[(/... OO.../),3],
+// these patterns are regex
+var patterns_1 = [ // make this move to win
+                  [(/ OO....../),0],[(/O..O.. ../),6],[(/......OO /),8],
+                  [(/.. ..O..O/),2],[(/ ..O..O../),0],[(/...... OO/),6],
+                  [(/..O..O.. /),8],[(/OO ....../),2],[(/ ...O...O/),0],
+                  [(/..O.O. ../),6],[(/O...O... /),8],[(/.. .O.O../),2],
+                  [(/O O....../),1],[(/O.. ..O../),3],[(/......O O/),7],
+                  [(/..O.. ..O/),5],[(/. ..O..O./),1],[(/... OO.../),3],
                   [(/.O..O.. ./),7],[(/...OO .../),5]
                  ];
 
-var patterns_2 = [
+var patterns_2 = [ // move to block a win
                   [(/  X . X  /),1],[(/ XX....../),0],[(/X..X.. ../),6],
                   [(/......XX /),8],[(/.. ..X..X/),2],[(/ ..X..X../),0],
                   [(/...... XX/),6],[(/..X..X.. /),8],[(/XX ....../),2],
@@ -24,12 +25,12 @@ var patterns_2 = [
                   [(/ ..X..  X/),6],[(/..X..  X /),8],[(/X  ..X.. /),2]
                  ];
 
-var patterns_3 = [
-                  [(/OOO....../),'O'], [(/...OOO.../),'O'], [(/......OOO/),'O'],
-                  [(/O..O..O../),'O'], [(/.O..O..O./),'O'], [(/..O..O..O/),'O'],
-                  [(/O...O...O/),'O'], [(/..O.O.O../),'O'], [(/XXX....../),'X'],
-                  [(/...XXX.../),'X'], [(/......XXX/),'X'], [(/X..X..X../),'X'],
-                  [(/.X..X..X./),'X'], [(/..X..X..X/),'X'], [(/X...X...X/),'X'],
+var patterns_3 = [ // if this pattern matches then the second value is the winner
+                  [(/OOO....../),'O'],[(/...OOO.../),'O'],[(/......OOO/),'O'],
+                  [(/O..O..O../),'O'],[(/.O..O..O./),'O'],[(/..O..O..O/),'O'],
+                  [(/O...O...O/),'O'],[(/..O.O.O../),'O'],[(/XXX....../),'X'],
+                  [(/...XXX.../),'X'],[(/......XXX/),'X'],[(/X..X..X../),'X'],
+                  [(/.X..X..X./),'X'],[(/..X..X..X/),'X'],[(/X...X...X/),'X'],
                   [(/..X.X.X../),'X']
                  ];
 
@@ -39,33 +40,41 @@ var player2 = 'O';
 var players = [player1, player2];
 var curr_turn = player1;
 
+// this searches patterns_1 then patterns_2 and if there is no match run get_move
+// (which is the first computer move)
 var comp = function(){
-  var x = get_pattern_1_move()
-    if(x === -1){
-      x = get_pattern_2_move()
-        if( x === -1){x = get_move()}
+  var x = get_pattern_1_move(board)
+  if(x === -1){
+    x = get_pattern_2_move()
+    if( x === -1){
+      x = get_move()
     }
+  }
   move(x,player2)
 }
 
+// this function makes the computers move
 var move = function(pos,x){
-  if(x!=curr_turn){return false}
-  if(+pos>=0&&+pos<=8&&!isNaN(+pos)&&board[+pos] === ' '){
+  if(x != curr_turn){return false}
+  if(+pos>=0 && +pos<=8 && !isNaN(+pos) && board[+pos] === ' '){
     board.splice(+pos,1,x)
-      curr_turn= (x === player1)? player2: player1
+      curr_turn = (x === player1) ? player2 : player1;
       return true
   }
   return false
 }
 
 var board_display = function(){
-  return ' '+board[0]+' |'+' '+board[1]+' |'+' '+board[2]+'\n===+===+===\n'+' '+board[3]+' |'+' '+board[4]+' |'+' '+board[5]+'\n===+===+===\n'+' '+board[6]+' |'+' '+board[7]+' |'+' '+board[8]
+  return ' '+board[0]+' |'+' '+board[1]+' |'+' '+board[2]+'\n===+===+===\n'+' '+
+             board[3]+' |'+' '+board[4]+' |'+' '+board[5]+'\n===+===+===\n'+' '+
+             board[6]+' |'+' '+board[7]+' |'+' '+board[8]
 }
 
 var show = function(){
   console.log(board_display())
 }
 
+// return true if the board is filled
 var board_filled = function(){
   var x = get_move() //rename this variable?
     if(x === -1){
@@ -76,6 +85,7 @@ var board_filled = function(){
   return false
 }
 
+// if the board matches a pattern from patterns_3, return the winner
 var winner = function(){
   var board_string = board.join('')
     var the_winner = null
@@ -88,22 +98,27 @@ var winner = function(){
   if(the_winner){
     show()
       console.log('Game over')
+      // console.log('Game over.', the_winner, 'is the winner!') Add this after tests
       return true
   }
   return false
 }
 
-var get_pattern_1_move = function(){
+// return the next move by matching a pattern from patterns_1 and returning the number
+// (the second value in the array)
+var get_pattern_1_move = function(board){
   var board_string = board.join('')
     for(var i = 0;i < patterns_1.length;i++){
       var array = board_string.match(patterns_1[i][0])
-        if(array){
+        if(array){ // this is only truthy when someone wins
           return patterns_1[i][1]
         }
     }
   return -1
 }
 
+// return the next move by matching a pattern from patterns_1 and returning the number
+// (the second value in the array)
 var get_pattern_2_move = function(){
   var board_string = board.join('')
     for(var i = 0;i < patterns_2.length;i++){
@@ -113,7 +128,9 @@ var get_pattern_2_move = function(){
   return -1
 }
 
-var get_move= function(){
+// if the center of board is free, go there, if not go to the first free space
+// this is only on the first move
+var get_move = function(){
   if(board[4] === ' '){
     return 4
   }
@@ -129,12 +146,24 @@ var play = function(){
   console.log("Enter [0-8]:")
   process.openStdin().on('data',function(res){
     if(move(res, player1)){
-      if(winner()||board_filled()) {exit()} else {
+      if(winner() || board_filled()) {
+        exit()
+      } else {
         comp()
-    if (winner()||board_filled()) {exit()} else {show()}
+        if (winner() || board_filled()) {
+          exit()
+        } else {
+          show()
+        }
       }
     }
   })
 }
 
 play()
+
+// exports needed for tests
+module.exports = {
+  get_pattern_1_move : get_pattern_1_move,
+  exit : exit
+}
--- tests/tape-tests.js
+++ tests/tape-tests.js
@@ -1,6 +1,127 @@
 var test = require('tape');
+var game = require('../game.js');
 
 test('First test here: ', function(t){
-  t.pass('just testing that everything works');
+  t.pass('testing that everything works');
+  t.end();
+});
+
+test('Testing that get_pattern_1_move() returns the correct next position', function(t){
+  var board = [' ', 'O', 'O', '.', '.', '.', '.', '.', '.'];
+  t.equals(game.get_pattern_1_move(board), 0, 'patterns_1[0][1] is returned when pattern_1[0][0] is matched');
+  t.end();
+});
+
+test('Testing that get_pattern_1_move() returns the correct next position', function(t){
+  var board = ['O', '.', '.', 'O', '.', '.', ' ', '.', '.'];
+  t.equals(game.get_pattern_1_move(board), 6, 'patterns_1[1][1] is returned when pattern_1[1][0] is matched');
+  t.end();
+});
+
+test('Testing that get_pattern_1_move() returns the correct next position', function(t){
+  var board = ['.', '.', '.', '.', '.', '.', 'O', 'O', ' '];
+  t.equals(game.get_pattern_1_move(board), 8, 'patterns_1[2][1] is returned when pattern_1[2][0] is matched');
+  t.end();
+});
+
+test('Testing that get_pattern_1_move() returns the correct next position', function(t){
+  var board = ['.', '.', ' ', '.', '.', 'O', '.', '.', 'O'];
+  t.equals(game.get_pattern_1_move(board), 2, 'patterns_1[3][1] is returned when pattern_1[3][0] is matched');
+  t.end();
+});
+
+test('Testing that get_pattern_1_move() returns the correct next position', function(t){
+  var board = [' ', '.', '.', 'O', '.', '.', 'O', '.', '.'];
+  t.equals(game.get_pattern_1_move(board), 0, 'patterns_1[4][1] is returned when pattern_1[4][0] is matched');
+  t.end();
+});
+
+test('Testing that get_pattern_1_move() returns the correct next position', function(t){
+  var board = ['.', '.', '.', '.', '.', '.', ' ', 'O', 'O'];
+  t.equals(game.get_pattern_1_move(board), 6, 'patterns_1[5][1] is returned when pattern_1[5][0] is matched');
+  t.end();
+});
+
+test('Testing that get_pattern_1_move() returns the correct next position', function(t){
+  var board = ['.', '.', 'O', '.', '.', 'O', '.', '.', ' '];
+  t.equals(game.get_pattern_1_move(board), 8, 'patterns_1[6][1] is returned when pattern_1[6][0] is matched');
+  t.end();
+});
+
+test('Testing that get_pattern_1_move() returns the correct next position', function(t){
+  var board = ['O', 'O', ' ', '.', '.', '.', '.', '.', '.'];
+  t.equals(game.get_pattern_1_move(board), 2, 'patterns_1[7][1] is returned when pattern_1[7][0] is matched');
+  t.end();
+});
+
+test('Testing that get_pattern_1_move() returns the correct next position', function(t){
+  var board = [' ', '.', '.', '.', 'O', '.', '.', '.', 'O'];
+  t.equals(game.get_pattern_1_move(board), 0, 'patterns_1[8][1] is returned when pattern_1[8][0] is matched');
+  t.end();
+});
+
+test('Testing that get_pattern_1_move() returns the correct next position', function(t){
+  var board = ['.', '.', 'O', '.', 'O', '.', ' ', '.', '.'];
+  t.equals(game.get_pattern_1_move(board), 6, 'patterns_1[9][1] is returned when pattern_1[9][0] is matched');
+  t.end();
+});
+
+test('Testing that get_pattern_1_move() returns the correct next position', function(t){
+  var board = ['O', '.', '.', '.', 'O', '.', '.', '.', ' '];
+  t.equals(game.get_pattern_1_move(board), 8, 'patterns_1[10][1] is returned when pattern_1[10][0] is matched');
+  t.end();
+});
+
+test('Testing that get_pattern_1_move() returns the correct next position', function(t){
+  var board = ['.', '.', ' ', '.', 'O', '.', 'O', '.', '.'];
+  t.equals(game.get_pattern_1_move(board), 2,'patterns_1[11][1] is returned when pattern_1[11][0] is matched');
+  t.end();
+});
+
+test('Testing that get_pattern_1_move() returns the correct next position', function(t){
+  var board = ['O', ' ', 'O', '.', '.', '.', '.', '.', '.'];
+  t.equals(game.get_pattern_1_move(board), 1, 'patterns_1[12][1] is returned when pattern_1[12][0] is matched');
+  t.end();
+});
+
+test('Testing that get_pattern_1_move() returns the correct next position', function(t){
+  var board = ['O', '.', '.', ' ', '.', '.', 'O', '.', '.'];
+  t.equals(game.get_pattern_1_move(board), 3, 'patterns_1[13][1] is returned when pattern_1[13][0] is matched');
+  t.end();
+});
+
+test('Testing that get_pattern_1_move() returns the correct next position', function(t){
+  var board = ['.', '.', '.', '.', '.', '.', 'O', ' ', 'O'];
+  t.equals(game.get_pattern_1_move(board), 7, 'patterns_1[14][1] is returned when pattern_1[14][0] is matched');
+  t.end();
+});
+
+test('Testing that get_pattern_1_move() returns the correct next position', function(t){
+  var board = ['.', '.', 'O', '.', '.', ' ', '.', '.', 'O'];
+  t.equals(game.get_pattern_1_move(board), 5, 'patterns_1[15][1] is returned when pattern_1[15][0] is matched');
+  t.end();
+});
+
+test('Testing that get_pattern_1_move() returns the correct next position', function(t){
+  var board = ['.', ' ', '.', '.', 'O', '.', '.', 'O', '.'];
+  t.equals(game.get_pattern_1_move(board), 1, 'patterns_1[16][1] is returned when pattern_1[16][0] is matched');
+  t.end();
+});
+
+test('Testing that get_pattern_1_move() returns the correct next position', function(t){
+  var board = ['.', '.', '.', ' ', 'O', 'O', '.', '.', '.'];
+  t.equals(game.get_pattern_1_move(board), 3, 'patterns_1[17][1] is returned when pattern_1[17][0] is matched');
+  t.end();
+});
+
+test('Testing that get_pattern_1_move() returns the correct next position', function(t){
+  var board = ['.', 'O', '.', '.', 'O', '.', '.', ' ', '.'];
+  t.equals(game.get_pattern_1_move(board), 7, 'patterns_1[18][1] is returned when pattern_1[18][0] is matched');
+  t.end();
+});
+
+test('Testing that get_pattern_1_move() returns the correct next position', function(t){
+  var board = ['.', '.', '.', 'O', 'O', ' ', '.', '.', '.'];
+  t.equals(game.get_pattern_1_move(board), 5, 'patterns_1[19][1] is returned when pattern_1[19][0] is matched');
   t.end();
 });
