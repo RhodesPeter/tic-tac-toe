RhodesPeter/tic-tac-toe

commit 7ae741d401dc374f837b35ad60270cb52b51cec4

rename move() to makeMove(). Related #10.

game.js
@@ -42,7 +42,7 @@ var player2 = 'O';
 var currentTurn = player1;
 
 // this searches winningMovePatterns then blockingPatterns and if there is no
-// match run firstMove (which is the first computer move)
+// match run firstMove()
 var findNextMove = function(){
   var nextPos = makeWinningMove(board);
   if(nextPos === -1){
@@ -54,10 +54,9 @@ var findNextMove = function(){
   move(nextPos,player2);
 };
 
-var move = function(pos, player){
+var makeMove = function(pos, player){
   if(player != currentTurn){return false;}
-  // + changes a buffer into an integer
-  if(+pos>=0 && +pos<=8 && !isNaN(+pos) && board[+pos] === ' '){
+  if(+pos >= 0 && +pos <= 8 && !isNaN(+pos) && board[+pos] === ' '){
     board.splice(+pos, 1, player);
     currentTurn = (player === player1) ? player2 : player1;
     return true;
@@ -86,7 +85,7 @@ var isBoardFilled = function(){
 };
 
 // if the board matches a pattern from hasWonPatterns, return the winner
-var winner = function(board){
+var hasSomeoneWon = function(board){
   var board_string = board.join('');
   var theWinner = null;
   for(var i = 0;i < hasWonPatterns.length;i++){
@@ -142,11 +141,11 @@ var startGame = function(){
   console.log("Enter [0-8]:");
   process.openStdin().on('data',function(res){
     if(move(res, player1)){
-      if(winner(board)[0] || isBoardFilled()) {
+      if(hasSomeoneWon(board)[0] || isBoardFilled()) {
         endGame();
       } else {
         findNextMove();
-        if (winner(board)[0] || isBoardFilled()) {
+        if (hasSomeoneWon(board)[0] || isBoardFilled()) {
           endGame();
         } else {
           logBoardToConsole();
@@ -162,5 +161,5 @@ startGame();
 module.exports = {
   makeWinningMove : makeWinningMove,
   makeBlockingMove : makeBlockingMove,
-  winner : winner
+  hasSomeoneWon : hasSomeoneWon
 };

tests/hasSomeoneWon.test.js
@@ -1,0 +1,121 @@
+var test = require('tape');
+var game = require('../game.js');
+
+test('Testing that hasSomeoneWon() returns the correct winning player', function(t){
+  var board = [' ', 'O', 'O', '.', '.', '.', '.', '.', ' '];
+  t.notOk(game.hasSomeoneWon(board)[0], 'false is returned when pattern is not matched in patterns_3');
+  t.equals(game.hasSomeoneWon(board)[1], null, 'test passed: O in the winner');
+  t.end();
+});
+
+test('Testing that hasSomeoneWon() returns the correct winning player', function(t){
+  var board = ['O', 'O', 'O', '.', '.', '.', '.', '.', ' '];
+  t.ok(game.hasSomeoneWon(board), 'true is returned when patterns_3[0][0] is matched');
+  t.equals(game.hasSomeoneWon(board)[1], 'O', 'test passed: O in the winner');
+  t.end();
+});
+
+test('Testing that hasSomeoneWon() returns the correct winning player', function(t){
+  var board = ['.', '.', '.', 'O', 'O', 'O', '.', '.', '.'];
+  t.ok(game.hasSomeoneWon(board), 'true is returned when patterns_3[1][0] is matched');
+  t.equals(game.hasSomeoneWon(board)[1], 'O', 'test passed: O in the winner');
+  t.end();
+});
+
+test('Testing that hasSomeoneWon() returns the correct winning player', function(t){
+  var board = ['.', '.', '.', '.', '.', '.', 'O', 'O', 'O'];
+  t.ok(game.hasSomeoneWon(board), 'true is returned when patterns_3[2][0] is matched');
+  t.equals(game.hasSomeoneWon(board)[1], 'O', 'test passed: O in the winner');
+  t.end();
+});
+
+test('Testing that hasSomeoneWon() returns the correct winning player', function(t){
+  var board = ['O', '.', '.', 'O', '.', '.', 'O', '.', '.'];
+  t.ok(game.hasSomeoneWon(board), 'true is returned when patterns_3[3][0] is matched');
+  t.equals(game.hasSomeoneWon(board)[1], 'O', 'test passed: O in the winner');
+  t.end();
+});
+
+test('Testing that hasSomeoneWon() returns the correct winning player', function(t){
+  var board = ['.', 'O', '.', '.', 'O', '.', '.', 'O', '.'];
+  t.ok(game.hasSomeoneWon(board), 'true is returned when patterns_3[4][0] is matched');
+  t.equals(game.hasSomeoneWon(board)[1], 'O', 'test passed: O in the winner');
+  t.end();
+});
+
+test('Testing that hasSomeoneWon() returns the correct winning player', function(t){
+  var board = ['.', '.', 'O', '.', '.', 'O', '.', '.', 'O'];
+  t.ok(game.hasSomeoneWon(board), 'true is returned when patterns_3[5][0] is matched');
+  t.equals(game.hasSomeoneWon(board)[1], 'O', 'test passed: O in the winner');
+  t.end();
+});
+
+test('Testing that hasSomeoneWon() returns the correct winning player', function(t){
+  var board = ['O', '.', '.', '.', 'O', '.', '.', '.', 'O'];
+  t.ok(game.hasSomeoneWon(board), 'true is returned when patterns_3[6][0] is matched');
+  t.equals(game.hasSomeoneWon(board)[1], 'O', 'test passed: O in the winner');
+  t.end();
+});
+
+test('Testing that hasSomeoneWon() returns the correct winning player', function(t){
+  var board = ['.', '.', 'O', '.', 'O', '.', 'O', '.', '.'];
+  t.ok(game.hasSomeoneWon(board), 'true is returned when patterns_3[7][0] is matched');
+  t.equals(game.hasSomeoneWon(board)[1], 'O', 'test passed: O in the winner');
+  t.end();
+});
+
+test('Testing that hasSomeoneWon() returns the correct winning player', function(t){
+  var board = ['X', 'X', 'X', '.', '.', '.', '.', '.', '.'];
+  t.ok(game.hasSomeoneWon(board), 'true is returned when patterns_3[8][0] is matched');
+  t.equals(game.hasSomeoneWon(board)[1], 'X', 'test passed: X in the winner');
+  t.end();
+});
+
+test('Testing that hasSomeoneWon() returns the correct winning player', function(t){
+  var board = ['.', '.', '.', 'X', 'X', 'X', '.', '.', '.'];
+  t.ok(game.hasSomeoneWon(board), 'true is returned when patterns_3[9][0] is matched');
+  t.equals(game.hasSomeoneWon(board)[1], 'X', 'test passed: X in the winner');
+  t.end();
+});
+
+test('Testing that hasSomeoneWon() returns the correct winning player', function(t){
+  var board = ['.', '.', '.', '.', '.', '.', 'X', 'X', 'X'];
+  t.ok(game.hasSomeoneWon(board), 'true is returned when patterns_3[10][0] is matched');
+  t.equals(game.hasSomeoneWon(board)[1], 'X', 'test passed: X in the winner');
+  t.end();
+});
+
+test('Testing that hasSomeoneWon() returns the correct winning player', function(t){
+  var board = ['X', '.', '.', 'X', '.', '.', 'X', '.', '.'];
+  t.ok(game.hasSomeoneWon(board), 'true is returned when patterns_3[11][0] is matched');
+  t.equals(game.hasSomeoneWon(board)[1], 'X', 'test passed: X in the winner');
+  t.end();
+});
+
+test('Testing that hasSomeoneWon() returns the correct winning player', function(t){
+  var board = ['.', 'X', '.', '.', 'X', '.', '.', 'X', '.'];
+  t.ok(game.hasSomeoneWon(board), 'true is returned when patterns_3[12][0] is matched');
+  t.equals(game.hasSomeoneWon(board)[1], 'X', 'test passed: X in the winner');
+  t.end();
+});
+
+test('Testing that hasSomeoneWon() returns the correct winning player', function(t){
+  var board = ['.', '.', 'X', '.', '.', 'X', '.', '.', 'X'];
+  t.ok(game.hasSomeoneWon(board), 'true is returned when patterns_3[13][0] is matched');
+  t.equals(game.hasSomeoneWon(board)[1], 'X', 'test passed: X in the winner');
+  t.end();
+});
+
+test('Testing that hasSomeoneWon() returns the correct winning player', function(t){
+  var board = ['X', '.', '.', '.', 'X', '.', '.', '.', 'X'];
+  t.ok(game.hasSomeoneWon(board), 'true is returned when patterns_3[14][0] is matched');
+  t.equals(game.hasSomeoneWon(board)[1], 'X', 'test passed: X in the winner');
+  t.end();
+});
+
+test('Testing that hasSomeoneWon() returns the correct winning player', function(t){
+  var board = ['.', '.', 'X', '.', 'X', '.', 'X', '.', '.'];
+  t.ok(game.hasSomeoneWon(board), 'true is returned when patterns_3[15][11] is matched');
+  t.equals(game.hasSomeoneWon(board)[1], 'X', 'test passed: X in the winner');
+  t.end();
+});

tests/winner.test.js
@@ -1,121 +1,0 @@
-var test = require('tape');
-var game = require('../game.js');
-
-test('Testing that winner() returns the correct winning player', function(t){
-  var board = [' ', 'O', 'O', '.', '.', '.', '.', '.', ' '];
-  t.notOk(game.winner(board)[0], 'false is returned when pattern is not matched in patterns_3');
-  t.equals(game.winner(board)[1], null, 'test passed: O in the winner');
-  t.end();
-});
-
-test('Testing that winner() returns the correct winning player', function(t){
-  var board = ['O', 'O', 'O', '.', '.', '.', '.', '.', ' '];
-  t.ok(game.winner(board), 'true is returned when patterns_3[0][0] is matched');
-  t.equals(game.winner(board)[1], 'O', 'test passed: O in the winner');
-  t.end();
-});
-
-test('Testing that winner() returns the correct winning player', function(t){
-  var board = ['.', '.', '.', 'O', 'O', 'O', '.', '.', '.'];
-  t.ok(game.winner(board), 'true is returned when patterns_3[1][0] is matched');
-  t.equals(game.winner(board)[1], 'O', 'test passed: O in the winner');
-  t.end();
-});
-
-test('Testing that winner() returns the correct winning player', function(t){
-  var board = ['.', '.', '.', '.', '.', '.', 'O', 'O', 'O'];
-  t.ok(game.winner(board), 'true is returned when patterns_3[2][0] is matched');
-  t.equals(game.winner(board)[1], 'O', 'test passed: O in the winner');
-  t.end();
-});
-
-test('Testing that winner() returns the correct winning player', function(t){
-  var board = ['O', '.', '.', 'O', '.', '.', 'O', '.', '.'];
-  t.ok(game.winner(board), 'true is returned when patterns_3[3][0] is matched');
-  t.equals(game.winner(board)[1], 'O', 'test passed: O in the winner');
-  t.end();
-});
-
-test('Testing that winner() returns the correct winning player', function(t){
-  var board = ['.', 'O', '.', '.', 'O', '.', '.', 'O', '.'];
-  t.ok(game.winner(board), 'true is returned when patterns_3[4][0] is matched');
-  t.equals(game.winner(board)[1], 'O', 'test passed: O in the winner');
-  t.end();
-});
-
-test('Testing that winner() returns the correct winning player', function(t){
-  var board = ['.', '.', 'O', '.', '.', 'O', '.', '.', 'O'];
-  t.ok(game.winner(board), 'true is returned when patterns_3[5][0] is matched');
-  t.equals(game.winner(board)[1], 'O', 'test passed: O in the winner');
-  t.end();
-});
-
-test('Testing that winner() returns the correct winning player', function(t){
-  var board = ['O', '.', '.', '.', 'O', '.', '.', '.', 'O'];
-  t.ok(game.winner(board), 'true is returned when patterns_3[6][0] is matched');
-  t.equals(game.winner(board)[1], 'O', 'test passed: O in the winner');
-  t.end();
-});
-
-test('Testing that winner() returns the correct winning player', function(t){
-  var board = ['.', '.', 'O', '.', 'O', '.', 'O', '.', '.'];
-  t.ok(game.winner(board), 'true is returned when patterns_3[7][0] is matched');
-  t.equals(game.winner(board)[1], 'O', 'test passed: O in the winner');
-  t.end();
-});
-
-test('Testing that winner() returns the correct winning player', function(t){
-  var board = ['X', 'X', 'X', '.', '.', '.', '.', '.', '.'];
-  t.ok(game.winner(board), 'true is returned when patterns_3[8][0] is matched');
-  t.equals(game.winner(board)[1], 'X', 'test passed: X in the winner');
-  t.end();
-});
-
-test('Testing that winner() returns the correct winning player', function(t){
-  var board = ['.', '.', '.', 'X', 'X', 'X', '.', '.', '.'];
-  t.ok(game.winner(board), 'true is returned when patterns_3[9][0] is matched');
-  t.equals(game.winner(board)[1], 'X', 'test passed: X in the winner');
-  t.end();
-});
-
-test('Testing that winner() returns the correct winning player', function(t){
-  var board = ['.', '.', '.', '.', '.', '.', 'X', 'X', 'X'];
-  t.ok(game.winner(board), 'true is returned when patterns_3[10][0] is matched');
-  t.equals(game.winner(board)[1], 'X', 'test passed: X in the winner');
-  t.end();
-});
-
-test('Testing that winner() returns the correct winning player', function(t){
-  var board = ['X', '.', '.', 'X', '.', '.', 'X', '.', '.'];
-  t.ok(game.winner(board), 'true is returned when patterns_3[11][0] is matched');
-  t.equals(game.winner(board)[1], 'X', 'test passed: X in the winner');
-  t.end();
-});
-
-test('Testing that winner() returns the correct winning player', function(t){
-  var board = ['.', 'X', '.', '.', 'X', '.', '.', 'X', '.'];
-  t.ok(game.winner(board), 'true is returned when patterns_3[12][0] is matched');
-  t.equals(game.winner(board)[1], 'X', 'test passed: X in the winner');
-  t.end();
-});
-
-test('Testing that winner() returns the correct winning player', function(t){
-  var board = ['.', '.', 'X', '.', '.', 'X', '.', '.', 'X'];
-  t.ok(game.winner(board), 'true is returned when patterns_3[13][0] is matched');
-  t.equals(game.winner(board)[1], 'X', 'test passed: X in the winner');
-  t.end();
-});
-
-test('Testing that winner() returns the correct winning player', function(t){
-  var board = ['X', '.', '.', '.', 'X', '.', '.', '.', 'X'];
-  t.ok(game.winner(board), 'true is returned when patterns_3[14][0] is matched');
-  t.equals(game.winner(board)[1], 'X', 'test passed: X in the winner');
-  t.end();
-});
-
-test('Testing that winner() returns the correct winning player', function(t){
-  var board = ['.', '.', 'X', '.', 'X', '.', 'X', '.', '.'];
-  t.ok(game.winner(board), 'true is returned when patterns_3[15][11] is matched');
-  t.equals(game.winner(board)[1], 'X', 'test passed: X in the winner');
-  t.end();
-});

commit f79d7aa5aa5532b0fb80ef26a52ec518d595c2f6

Fix bug which was introduced when refactoring var names.
This bug was introduced with commit 7ae741d401dc374f837b35ad60270cb52b51cec4
as I re named move() to makeMove() but did not change all of the function calls to move().

game.js
@@ -51,7 +51,7 @@ var findNextMove = function(){
       nextPos = firstMove();
     }
   }
-  move(nextPos,player2);
+  makeMove(nextPos,player2);
 };
 
 var makeMove = function(pos, player){
@@ -140,7 +140,7 @@ var startGame = function(){
   logBoardToConsole();
   console.log("Enter [0-8]:");
   process.openStdin().on('data',function(res){
-    if(move(res, player1)){
+    if(makeMove(res, player1)){
       if(hasSomeoneWon(board)[0] || isBoardFilled()) {
         endGame();
       } else {