6326d2f41ebb7d71dd9120680f8d0d6f58b388f2
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-03-10 20:50:51 +0000

    Refactor the code which adds event listeners to the game boxes.
    Related #10.

--- public/index.js
+++ public/index.js
@@ -33,7 +33,7 @@ function addListenerToChallengeButtons(i){
     else if (gameState.player1 === ''){
       addSymbol(symbol, 'player1');
       swapVisibility(symbolContainer, game);
-      startMessage()
+      startMessage();
     }
     else if (gameState.player1 === symbol){
       player.innerHTML = 'That symbol has been taken!';
@@ -41,7 +41,7 @@ function addListenerToChallengeButtons(i){
     else {
       addSymbol(symbol, 'player2');
       swapVisibility(symbolContainer, game);
-      startMessage()
+      startMessage();
     }
   });
 }
@@ -93,3 +93,206 @@ function show(element){
   element.classList.add("visible");
   element.classList.remove("hidden");
 }
+
+var box = document.getElementsByClassName("box");
+
+for(var i = 0; i < box.length; i++) {
+  addListenersToBoxes(i);
+}
+
+function addListenersToBoxes(i){
+  box[i].addEventListener("click", function() {
+    if (gameState.challenge === 'Human vs. Human'){
+      gameState.clickedPosition = i;
+      humanVsHuman(gameState.clickedPosition);
+    }
+    else if (currentTurn === 'player1'){
+      gameState.clickedPosition = i;
+      humanVsComputer(gameState.clickedPosition);
+    }
+  });
+}
+
+// regex matches if current player is 1 move away from winning & supplies winning move
+var winningMovePatterns = [
+                           [(/ OO....../),0],[(/O..O.. ../),6],[(/......OO /),8],
+                           [(/.. ..O..O/),2],[(/ ..O..O../),0],[(/...... OO/),6],
+                           [(/..O..O.. /),8],[(/OO ....../),2],[(/ ...O...O/),0],
+                           [(/..O.O. ../),6],[(/O...O... /),8],[(/.. .O.O../),2],
+                           [(/O O....../),1],[(/O.. ..O../),3],[(/......O O/),7],
+                           [(/..O.. ..O/),5],[(/. ..O..O./),1],[(/... OO.../),3],
+                           [(/.O..O.. ./),7],[(/...OO .../),5]
+                          ];
+
+// regex matches if opponent is 1 move away from winning & supplies blocking move
+var blockingPatterns = [
+                        [(/  X . X  /),1],[(/ XX....../),0],[(/X..X.. ../),6],
+                        [(/......XX /),8],[(/.. ..X..X/),2],[(/ ..X..X../),0],
+                        [(/...... XX/),6],[(/..X..X.. /),8],[(/XX ....../),2],
+                        [(/.. .X...X/),2],[(/..X.X. ../),6],[(/X...X... /),8],
+                        [(/.. .X.X../),2],[(/X X....../),1],[(/X.. ..X../),3],
+                        [(/......X X/),7],[(/..X.. ..X/),5],[(/. ..X..X./),1],
+                        [(/... XX.../),3],[(/.X..X.. ./),7],[(/...XX .../),5],
+                        [(/ X X.. ../),0],[(/ ..X.. X /),6],[(/.. ..X X /),8],
+                        [(/ X ..X.. /),2],[(/  XX.. ../),0],[(/X.. .. X /),6],
+                        [(/.. .XX   /),8],[(/X  ..X.. /),2],[(/ X  ..X../),0],
+                        [(/ ..X..  X/),6],[(/..X..  X /),8],[(/X  ..X.. /),2]
+                       ];
+
+// regex matches if a player has won & supplies the winning player
+var hasWonPatterns = [
+                      [(/OOO....../),'O'],[(/...OOO.../),'O'],[(/......OOO/),'O'],
+                      [(/O..O..O../),'O'],[(/.O..O..O./),'O'],[(/..O..O..O/),'O'],
+                      [(/O...O...O/),'O'],[(/..O.O.O../),'O'],[(/XXX....../),'X'],
+                      [(/...XXX.../),'X'],[(/......XXX/),'X'],[(/X..X..X../),'X'],
+                      [(/.X..X..X./),'X'],[(/..X..X..X/),'X'],[(/X...X...X/),'X'],
+                      [(/..X.X.X../),'X']
+                     ];
+
+function markBox(pos, player){
+  box[pos].innerHTML = gameState[player];
+};
+
+function startMessage(){
+  console.log("Click a square to make your first move!");
+};
+
+function humanVsComputer(pos){
+  if (gameState.player1 === 'O'){
+    gameState.player1 = 'X';
+    gameState.player2 = 'O';
+  }
+
+  makeMove(pos, gameState[currentTurn]);
+  hasSomeoneWon()[0];
+
+  if (isBoardFilled()){ return; };
+
+  setTimeout(function(){
+    findNextMove(gameState.board);
+    hasSomeoneWon()[0];
+  }, 500);
+
+  if (isBoardFilled()){ return; };
+
+};
+
+function compVsComp(){
+  setTimeout(function(){
+    findNextMove(gameState.board);
+    if (hasSomeoneWon()[0]){ return; }
+    if (isBoardFilled()){ return; }
+    compVsComp();
+  }, 500);
+};
+
+function humanVsHuman(pos){
+  if (gameState.player1 === 'O'){
+    gameState.player1 = 'X';
+    gameState.player2 = 'O';
+  };
+  makeMove(pos, gameState[currentTurn]);
+  hasSomeoneWon()[0];
+  if (isBoardFilled()){
+    return;
+  };
+}
+
+var makeMove = function(pos, symbol){
+  if(gameState.board[pos] === ' '){
+    gameState.board.splice(pos, 1, symbol);
+    currentTurn = (currentTurn === 'player1') ? 'player2' : 'player1';
+    logBoardToConsole(gameState.board);
+  }
+};
+
+var findNextMove = function(board){
+  var nextPos = makeWinningMove(board);
+  if(nextPos === -1){
+    nextPos = makeBlockingMove(board);
+    if(nextPos === -1){
+      nextPos = defaultMove(board);
+    }
+  }
+  makeMove(nextPos, gameState[currentTurn]);
+};
+
+function mapSymbolToPatterns(str){
+  var replace = gameState.player1;
+  var symbol = new RegExp(replace,"g");
+  str = str.replace(symbol, "X");
+  return str;
+};
+
+// if the board matches a pattern from hasWonPatterns, return the winner
+var hasSomeoneWon = function(){
+  var board_string = gameState.board.join('');
+  var board_string = mapSymbolToPatterns(board_string);
+
+  var theWinner = null;
+  for(var i = 0;i < hasWonPatterns.length;i++){
+    var array = board_string.match(hasWonPatterns[i][0]);
+    if(array){
+      theWinner = hasWonPatterns[i][1];
+    }
+  }
+  if(theWinner){
+    var board = gameState.board
+    logBoardToConsole(board);
+    console.log('Game over.', theWinner, 'is the winner!');
+    return [true, theWinner];
+  }
+  return [false, null];
+};
+
+// return true if the board is filled
+var isBoardFilled = function(){
+  if(gameState.board.indexOf(' ') === -1){
+    console.log("Game over, it's a draw!");
+    return true;
+  }
+  return false;
+};
+
+// return the next move by matching a pattern from winningMovePatterns and returning the number
+// (the second value in the array)
+var makeWinningMove = function(board){
+  var board_string = board.join('');
+  var board_string = mapSymbolToPatterns(board_string);
+
+  for(var i = 0;i < winningMovePatterns.length;i++){
+    var array = board_string.match(winningMovePatterns[i][0]);
+    if(array){ // this is only truthy when someone wins
+      return winningMovePatterns[i][1];
+    }
+  }
+  return -1;
+};
+
+// return the next move by matching a pattern from winningMovePatterns and returning the number
+// (the second value in the array)
+var makeBlockingMove = function(board){
+  var board_string = board.join('');
+  var board_string = mapSymbolToPatterns(board_string);
+
+  for(var i = 0;i < blockingPatterns.length;i++){
+    var array = board_string.match(blockingPatterns[i][0]);
+      if(array){return blockingPatterns[i][1];}
+  }
+  return -1;
+};
+
+// if the center of board is free, go there, if not go to position 0
+var defaultMove = function(board){
+  return board[4] === ' ' ? 4 : board.indexOf(' ');
+};
+
+var logBoardToConsole = function(board){
+    console.log(board_display(board));
+};
+
+var board_display = function(board){
+  return ' '+board[0]+' |'+' '+board[1]+' |'+' '+board[2]+'\n===+===+===\n'+' '+
+             board[3]+' |'+' '+board[4]+' |'+' '+board[5]+'\n===+===+===\n'+' '+
+             board[6]+' |'+' '+board[7]+' |'+' '+board[8];
+};
